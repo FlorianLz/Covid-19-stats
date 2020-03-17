af1fa9190edc861c812fc9ceacdcd03a25f8cc62
FlorianLz <46797434+florianlz@users.noreply.github.com>
2020-03-17 17:57:31 +0100

    Automatic selection of France when loading.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Liste from "./Liste";
 import Evolution from "./Evolution";
 
 function App() {
-  //const[latestDeaths, setLatestDeaths] = useState('Loading...');
   const[numberOfCountry, setNumberOfCountry] = useState(0);
   const[data, setData] = useState([]);
   const[dataConfirmed, setDataConfirmed] = useState([]);
@@ -18,6 +17,7 @@ function App() {
   const[actualCountry,setActualCountry] = useState('France');
   const[isLoadingDeaths,setIsLoadingDeaths] = useState(false);
   const[isLoadingConfirmed,setIsLoadingConfirmed] = useState(false);
+  const[idFrance,setIdFrance] = useState(0);
 
   let jsxList=[];
   let jsxEvolutionFrance=[];
@@ -33,17 +33,11 @@ function App() {
   }
 
   let jsxDate =day + '/'+ month + '/' +thedate.getFullYear()+ ' - '+ thedate.getHours()+'h'+ thedate.getMinutes();
-  //let jsxList = <Liste nombre={numberOfCountry} />;
 
-  /*let jsxList = data.map((p,index) => <Liste
-      id={index}
-      number={numberOfCountry}
-      country={p.country}
-      province={p.province}
-  />);*/
 
   for(let i =0;i<numberOfCountry;i++){
     jsxList.push(<Liste
+        key={i}
         id={i}
         number={numberOfCountry}
         country={data.locations[i].country}
@@ -52,7 +46,7 @@ function App() {
     />)
   }
 
-  if(evolutionFranceDeaths.length == evolutionFranceConfirmed.length && evolutionFranceConfirmed.length >= 52){
+  if(evolutionFranceDeaths.length === evolutionFranceConfirmed.length && evolutionFranceConfirmed.length >= 52){
     jsxEvolutionFrance.push(evolutionFranceConfirmed.map((p,index) => <Evolution
         date={p.date}
         number={p.number}
@@ -61,118 +55,122 @@ function App() {
     />));
   }
 
-  let jsxEvolutionFranceConfirmed = evolutionFranceConfirmed.map((p,index) => <Evolution
-      date={p.date}
-      number={p.number}
-      key={index}
-      //deaths={evolutionFranceDeaths.number}
-    />);
-
-  let jsxEvolutionFranceDeaths = evolutionFranceDeaths.map((p,index) => <Evolution
-      date={p.date}
-      number={p.number}
-      key={index}
-      //deaths={evolutionFranceDeaths.number}
-  />);
-
-
-  async function getLatestDeaths(nb) {
+  async function getLatestDeaths(nb,first) {
     const data = (await axios.get('https://coronavirus-tracker-api.herokuapp.com/deaths')).data;
-    //setLatestDeaths(data.latest);
     setData(data);
-    setDataFrance(data.locations[nb]);
-    //console.log(data.locations[159]);
     setNumberOfCountry(data.locations.length);
     setLastUpdate(data.last_updated);
-    //console.log(lastUpdate);
 
-    setDataFranceDeaths(data.locations[nb]);
-    console.log(data.locations[nb].history);
-    let object=data.locations[nb].history;
+    if(first===true){
+      for(let i=0;i<data.locations.length;i++){
+        if(data.locations[i].country === 'France' && data.locations[i].province === 'France'){
+          setIdFrance(i);
+          setDataFrance(data.locations[i]);
+          setDataFranceDeaths(data.locations[i]);
+          let object=data.locations[i].history;
+          end(object);
+        }
+      }
+    }else{
+      setDataFrance(data.locations[nb]);
+      setDataFranceDeaths(data.locations[nb]);
+      let object=data.locations[nb].history;
+      end(object);
+    }
 
-    let tab = [];
-    for (let key in object) {
-      let obj = {};
-      obj["date"] = key;
-      obj["number"] = object[key];
-      tab.push(obj);
-    }
-    let tabInOrder = tab.sort((a, b) => new Date(b.date) - new Date(a.date)).reverse();
-    let id=0;
-    for(let i=0;i<tabInOrder.length;i++){
-      let thedate=new Date (tabInOrder[i].date);
-      let day = thedate.getDate();
-      let month = thedate.getMonth()+1;
-      let year = thedate.getFullYear();
-      if(month<10){
-        month='0'+month;
+    function end(object){
+      let tab = [];
+      for (let key in object) {
+        let obj = {};
+        obj["date"] = key;
+        obj["number"] = object[key];
+        tab.push(obj);
       }
-      if(day<10){
-        day='0'+day;
+      let tabInOrder = tab.sort((a, b) => new Date(b.date) - new Date(a.date)).reverse();
+      let id=0;
+      for(let i=0;i<tabInOrder.length;i++){
+        let thedate=new Date (tabInOrder[i].date);
+        let day = thedate.getDate();
+        let month = thedate.getMonth()+1;
+        let year = thedate.getFullYear();
+        if(month<10){
+          month='0'+month;
+        }
+        if(day<10){
+          day='0'+day;
+        }
+        tabInOrder[i].date = day + '/'+ month + '/' +year;
+        tabInOrder[i].key=id;
+        id++;
       }
-      let finaldate =day + '/'+ month + '/' +year;
-      tabInOrder[i].date = finaldate;
-      tabInOrder[i].key=id;
-      id++;
+      setEvolutionFranceDeaths(tabInOrder);
+      setIsLoadingDeaths(true);
     }
-    setEvolutionFranceDeaths(tabInOrder);
-    console.log(tabInOrder);
-    //for(let i=0;i<tabInOrder.length;i++){
-      //setDataFranceDeaths(dataFranceDeaths.push(tabInOrder[i].number));
-    //}
-    console.log(dataFranceDeaths);
-    setIsLoadingDeaths(true);
   }
 
-  async function getLatestConfirmed(nb) {
+  async function getLatestConfirmed(nb,first) {
     const data = (await axios.get('https://coronavirus-tracker-api.herokuapp.com/confirmed')).data;
     setDataConfirmed(data);
-    setDataFranceConfirmed(data.locations[nb]);
     setLastUpdate(data.last_updated);
-    //console.log(data.locations[159].history);
-    let object=data.locations[nb].history;
 
-    let tab = [];
-    for (let key in object) {
-      let obj = {};
-      obj["date"] = key;
-      obj["number"] = object[key];
-      tab.push(obj);
-    }
-    let tabInOrder = tab.sort((a, b) => new Date(b.date) - new Date(a.date)).reverse();
-    let id=0;
-    for(let i=0;i<tabInOrder.length;i++){
-      let thedate=new Date (tabInOrder[i].date);
-      let day = thedate.getDate();
-      let month = thedate.getMonth()+1;
-      let year = thedate.getFullYear();
-      if(month<10){
-        month='0'+month;
+    if(first===true){
+      for(let i=0;i<data.locations.length;i++){
+        if(data.locations[i].country === 'France' && data.locations[i].province === 'France') {
+          setIdFrance(i);
+          setDataFranceConfirmed(data.locations[i]);
+          let object = data.locations[i].history;
+          end(object);
+        }
       }
-      if(day<10){
-        day='0'+day;
-      }
-      let finaldate =day + '/'+ month + '/' +year;
-      tabInOrder[i].date = finaldate;
-      tabInOrder[i].key=id;
-      id++;
+    }else{
+      setDataFranceConfirmed(data.locations[nb]);
+      let object=data.locations[nb].history;
+      end(object);
     }
-    setEvolutionFranceConfirmed(tabInOrder);
-    //console.log(tabInOrder);
-    setIsLoadingConfirmed(true);
+
+    function end(object){
+      let tab = [];
+      for (let key in object) {
+        let obj = {};
+        obj["date"] = key;
+        obj["number"] = object[key];
+        tab.push(obj);
+      }
+      let tabInOrder = tab.sort((a, b) => new Date(b.date) - new Date(a.date)).reverse();
+      let id=0;
+      for(let i=0;i<tabInOrder.length;i++){
+        let thedate=new Date (tabInOrder[i].date);
+        let day = thedate.getDate();
+        let month = thedate.getMonth()+1;
+        let year = thedate.getFullYear();
+        if(month<10){
+          month='0'+month;
+        }
+        if(day<10){
+          day='0'+day;
+        }
+        tabInOrder[i].date = day + '/'+ month + '/' +year;
+        tabInOrder[i].key=id;
+        id++;
+      }
+      setEvolutionFranceConfirmed(tabInOrder);
+      setIsLoadingConfirmed(true);
+    }
+
+
   }
 
 
   useEffect(() => {
-    getLatestDeaths(157);
-    getLatestConfirmed(157);
+    getLatestDeaths(0,true);
+    getLatestConfirmed(0,true);
   },[]);
 
   function actualiser(e) {
     let nb = e.target.options[e.target.selectedIndex].id;
     setActualCountry(e.target.options[e.target.selectedIndex].value);
-    getLatestDeaths(nb);
-    getLatestConfirmed(nb);
+    getLatestDeaths(nb,false);
+    getLatestConfirmed(nb,false);
 
   }
 
@@ -200,27 +198,31 @@ function App() {
           <h2>Deaths :</h2>
 
           <table>
-            <tr>
-              <td>World :</td>
-              <td>{data.latest}</td>
-            </tr>
-            <tr>
-              <td>{actualCountry} :</td>
-              <td>{dataFrance.latest}</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>World :</td>
+                <td>{data.latest}</td>
+              </tr>
+              <tr>
+                <td>{actualCountry} :</td>
+                <td>{dataFrance.latest}</td>
+              </tr>
+            </tbody>
           </table>
 
           <h2>Confirmed Cases :</h2>
 
           <table>
-            <tr>
-              <td>World :</td>
-              <td>{dataConfirmed.latest}</td>
-            </tr>
-            <tr>
-              <td>{actualCountry} :</td>
-              <td>{dataFranceConfirmed.latest}</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>World :</td>
+                <td>{dataConfirmed.latest}</td>
+              </tr>
+              <tr>
+                <td>{actualCountry} :</td>
+                <td>{dataFranceConfirmed.latest}</td>
+              </tr>
+            </tbody>
           </table>
 
           <h2>Increasing stats in
@@ -229,13 +231,15 @@ function App() {
             </select>
           </h2>
           <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Confirmed cases</th>
+                <th>Deaths</th>
+              </tr>
+            </thead>
             <tbody>
-            <tr>
-              <td>Date</td>
-              <td>Confirmed cases</td>
-              <td>Deaths</td>
-            </tr>
-            {jsxEvolutionFrance}
+              {jsxEvolutionFrance}
             </tbody>
           </table>
         </div>
--- src/Liste.js
+++ src/Liste.js
@@ -3,16 +3,16 @@ import React from 'react';
 function Liste(props) {
     if(!props.province){
         return (
-            <option id={props.id} value={props.country} onClick={props.actualiser}>{props.country}</option>
+            <option key={props.id} id={props.id} value={props.country} onClick={props.actualiser}>{props.country}</option>
         );
     }else{
         if(props.country==='France' && props.province==='France'){
             return (
-                <option id={props.id} value={props.country+props.province} onClick={props.actualiser} selected={"selected"}>{props.country} ({props.province})</option>
+                <option key={props.id} id={props.id} value={props.country+props.province} onClick={props.actualiser} selected>{props.country} ({props.province})</option>
             );
         }else{
             return (
-                <option id={props.id} value={props.country +' ('+props.province+')'} onClick={props.actualiser}>{props.country} ({props.province})</option>
+                <option key={props.id} id={props.id} value={props.country +' ('+props.province+')'} onClick={props.actualiser}>{props.country} ({props.province})</option>
             );
         }
     }
